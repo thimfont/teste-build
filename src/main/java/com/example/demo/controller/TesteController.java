de66b3e4a5616613e6b31a0c718485764c268ece
Thiago <thimfont@gmail.com>
2019-08-29 17:00:26 -0300

    disponibilizando controller para teste.

---
+++ src/main/java/com/example/demo/controller/TesteController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "teste")
+public class TesteController {
+
+    @GetMapping
+    public String teste() {
+        return "Funcionou";
+    }
+}
